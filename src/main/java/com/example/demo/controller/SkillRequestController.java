c63abe7a3d136623dcb00cc5fda9a7ca2de3e177
bala-amypo <bala@amypo.in>
2025-12-18 17:20:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillRequestController.java
+++ src/main/java/com/example/demo/controller/SkillRequestController.java
@@ -7,26 +7,26 @@ import com.example.demo.service.SkillRequestService;
 import com.example.demo.model.SkillRequest;
 
 @RestController
-@RequestMapping("/api/skill-reuqests/skill-requests")
+@RequestMapping("/api/skill-requests/POST")
 public class SkillRequestController {
     @Autowired SkillRequestService service;
-    @PostMapping
+    @PostMapping(/api/skill-requests/)
     public SkillRequest create(@RequestBody SkillRequest request) {
         return service.createRequest(request);
     }
-    @GetMapping("/{id}")
+    @GetMapping("/api/skill-requests/PUT/{id}")
     public SkillRequest getById(@PathVariable Long id) {
         return service.getRequestById(id);
     }
-    @GetMapping("/user/{userId}")
+    @GetMapping("/api/skill-requests/GET/{Id}")
     public List<SkillRequest> getByUser(@PathVariable Long userId) {
         return service.getRequestsByUser(userId);
     }
-    @PutMapping("/{id}")
+    @PutMapping("/api/skill-requests/GET/user/{userid}")
     public SkillRequest update(@PathVariable Long id, @RequestBody SkillRequest request) {
         return service.updateRequest(id, request);
     }
-    @PutMapping("/{id}/deactivate")
+    @PutMapping("/api/skill-requests/PUT/{id}/deactivate")
     public void deactivate(@PathVariable Long id) {
         service.deactivateRequest(id);
     }
