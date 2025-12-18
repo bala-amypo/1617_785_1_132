9d89b619a23d536960ec37d843f3a4c71c2be929
bala-amypo <bala@amypo.in>
2025-12-18 14:56:29 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -16,4 +16,54 @@ public class Skill{
     private String category;
     private String description;
     private Boolean active;
+    public Skill() {
+    }
+    public Skill(Long id, String name, String category, String description, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
 }
