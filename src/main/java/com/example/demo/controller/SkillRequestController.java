8e974484674129e07348f5f4e69f4b5752caaa45
bala-amypo <bala@amypo.in>
2025-12-18 16:47:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillRequestController.java
+++ src/main/java/com/example/demo/controller/SkillRequestController.java
@@ -1,8 +1,33 @@
 package com.exmaple.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.exmaple.demo.service.SkillRequestService;
+import com.exmaple.demo.model.SkillRequest;
 
 @RestController
-public class SkillRequestController{
-    
-} 
+@RequestMapping("/skill-requests")
+public class SkillRequestController {
+    @Autowired SkillRequestService service;
+    @PostMapping
+    public SkillRequest create(@RequestBody SkillRequest request) {
+        return service.createRequest(request);
+    }
+    @GetMapping("/{id}")
+    public SkillRequest getById(@PathVariable Long id) {
+        return service.getRequestById(id);
+    }
+    @GetMapping("/user/{userId}")
+    public List<SkillRequest> getByUser(@PathVariable Long userId) {
+        return service.getRequestsByUser(userId);
+    }
+    @PutMapping("/{id}")
+    public SkillRequest update(@PathVariable Long id, @RequestBody SkillRequest request) {
+        return service.updateRequest(id, request);
+    }
+    @PutMapping("/{id}/deactivate")
+    public void deactivate(@PathVariable Long id) {
+        service.deactivateRequest(id);
+    }
+}
