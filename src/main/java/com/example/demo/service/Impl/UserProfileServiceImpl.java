e021da7c6dd2c96f9d57c729be144fed51f7e80e
bala-amypo <bala@amypo.in>
2025-12-18 16:09:33 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserProfileServiceImpl.java
@@ -1,19 +1,3 @@
-package com.exmaple.demo.service.Impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import orng.springframework.stereotype.Service;
-import com.exmaple.demo.service.UserProfileService;
-import com.example.demo.model.UserProfile;
-
-@Service
-public class UserProfileServieImpl implements UserProfileService{
-    @Autowired UserProfileRepository upr;
-    @Override
-    public UserProfile createUser(UserProfile up){
-        return upr.save(str);
-    }
-
-} 
 package com.exmaple.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,30 +16,22 @@ public class UserProfileServiceImpl implements UserProfileService{
         }
         return upr.save(user);
     }
-
     public UserProfile updateUser(Long id,UserProfile user){
-        UserProfile existing=upr.findById(id)
-            .orElseThrow(()->new RuntimeException("UserProfile not found"));
+        UserProfile existing=upr.findById(id).orElseThrow(()->new RuntimeException("UserProfile not found"));
 
         existing.setFullName(user.getFullName());
         existing.setEmail(user.getEmail());
 
         return upr.save(existing);
     }
-
     public UserProfile getUserById(Long id){
-        return upr.findById(id)
-            .orElseThrow(()->new RuntimeException("UserProfile not found"));
+        return upr.findById(id).orElseThrow(()->new RuntimeException("UserProfile not found"));
     }
-
     public List<UserProfile> getAllUsers(){
         return upr.findAll();
     }
-
     public void deactivateUser(Long id){
-        UserProfile user=upr.findById(id)
-            .orElseThrow(()->new RuntimeException("UserProfile not found"));
-
+        UserProfile user=upr.findById(id).orElseThrow(()->new RuntimeException("UserProfile not found"));
         user.setActive(false);
         upr.save(user);
     }
