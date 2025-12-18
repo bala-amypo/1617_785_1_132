57a779dec279f4a9d75f7c587e7756e5c8e03b5b
bala-amypo <bala@amypo.in>
2025-12-18 17:14:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillController.java
+++ src/main/java/com/example/demo/controller/SkillController.java
@@ -14,7 +14,7 @@ import com.example.demo.service.SkillService;
 @RestController
 public class SkillController{
     @Autowired SkillService ss;
-    @PostMapping("/skills")
+    @PostMapping("/POST")
     public Skill create(@RequestBody Skill skill){
         return ss.createSkill(skill);
     }
