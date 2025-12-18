d9eae860a10a77cbacc34fb4fd258f54fad528e3
bala-amypo <bala@amypo.in>
2025-12-18 17:08:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/MatchRecordRepository.java
+++ src/main/java/com/example/demo/repository/MatchRecordRepository.java
@@ -1,9 +1,9 @@
-package com.exmaple.demo.repository;
+package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import com.exmaple.demo.model.MatchRecord;
+import com.example.demo.model.MatchRecord;
 
 @Repository
 public interface MatchRecordRepository extends JpaRepository<MatchRecord, Long> {
