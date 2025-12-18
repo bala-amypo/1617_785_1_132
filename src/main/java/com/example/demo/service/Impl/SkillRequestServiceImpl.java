cb6b479d9198a0417e2520382cbef5431b17ccad
bala-amypo <bala@amypo.in>
2025-12-18 16:46:32 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillRequestServiceImpl.java
@@ -1,16 +1,6 @@
-package com.exmaple.demo.service.Impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import orng.springframework.stereotype.Service;
-import com.exmaple.demo.service.SkillRequestService;
-import com.example.demo.model.SkillRequest;
-
-@Service
-public class SkillRequestServieImpl implements SkillRequestServie{
-    @Autowired SkillRequestRepository ;
-} 
 package com.exmaple.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import com.exmaple.demo.service.SkillRequestService;
@@ -18,37 +8,30 @@ import com.exmaple.demo.repository.SkillRequestRepository;
 import com.exmaple.demo.model.SkillRequest;
 
 @Service
-public class SkillRequestServiceImpl implements SkillRequestService{
-
-    private final SkillRequestRepository repo;
-
-    public SkillRequestServiceImpl(SkillRequestRepository repo){
-        this.repo=repo;
-    }
-
-    public SkillRequest createRequest(SkillRequest request){
+public class SkillRequestServiceImpl implements SkillRequestService {
+    @Autowired SkillRequestRepository repo;
+    @Override
+    public SkillRequest createRequest(SkillRequest request) {
         return repo.save(request);
     }
-
-    public SkillRequest updateRequest(Long id,SkillRequest request){
-        SkillRequest existing=repo.findById(id)
-                .orElseThrow(()->new RuntimeException("Request not found"));
+    @Override
+    public SkillRequest updateRequest(Long id, SkillRequest request) {
+        SkillRequest existing = repo.findById(id).orElseThrow(() -> new RuntimeException("Request not found"));
         existing.setSkill(request.getSkill());
         existing.setActive(request.getActive());
         return repo.save(existing);
     }
-
-    public SkillRequest getRequestById(Long id){
-        return repo.findById(id)
-                .orElseThrow(()->new RuntimeException("Request not found"));
+    @Override
+    public SkillRequest getRequestById(Long id) {
+        return repo.findById(id).orElseThrow(() -> new RuntimeException("Request not found"));
     }
-
-    public List<SkillRequest> getRequestsByUser(Long userId){
+    @Override
+    public List<SkillRequest> getRequestsByUser(Long userId) {
         return repo.findByUserId(userId);
     }
-
-    public void deactivateRequest(Long id){
-        SkillRequest req=getRequestById(id);
+    @Override
+    public void deactivateRequest(Long id) {
+        SkillRequest req = getRequestById(id);
         req.setActive(false);
         repo.save(req);
     }
