1865222c851401a0b496e190d814185b2a1a7060
bala-amypo <bala@amypo.in>
2025-12-18 17:09:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRequestRepository.java
+++ src/main/java/com/example/demo/repository/SkillRequestRepository.java
@@ -1,9 +1,9 @@
-package com.exmaple.demo.repository;
+package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import com.exmaple.demo.model.SkillRequest;
+import com.example.demo.model.SkillRequest;
 
 @Repository
 public interface SkillRequestRepository extends JpaRepository<SkillRequest,Long>{
