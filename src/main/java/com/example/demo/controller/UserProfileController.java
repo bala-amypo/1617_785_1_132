d8a01abff49d7805c9f46125a09b2ee9eaff990f
bala-amypo <bala@amypo.in>
2025-12-18 17:05:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserProfileController.java
+++ src/main/java/com/example/demo/controller/UserProfileController.java
@@ -1,23 +1,5 @@
 package com.exmaple.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.example.demo.model.UserProfile;
-import com.example.demo.service.UserProfileService;
-
-@RestController
-public class UserProfileController{
-    @Autowired UserProfileService ups;
-    @PostMapping("/POST")
-    public UserProfile createuserprofile(@RequestBody UserProfile up){
-        return ups.createUser(up);
-    }
-
-}
-package com.exmaple.demo.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
