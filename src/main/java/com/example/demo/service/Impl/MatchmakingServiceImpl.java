27a63922326fee36c689ed14f9d61ca2c0b00df3
bala-amypo <bala@amypo.in>
2025-12-18 16:50:29 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/MatchmakingServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/MatchmakingServiceImpl.java
@@ -9,3 +9,12 @@ import com.example.demo.model.MatchRecord;
 public class MatchmakingServiceImpl implements MatchmakingService{
     @Autowired MatchRecordRepository ;
 }
+package com.exmaple.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import com.exmaple.demo.model.MatchRecord;
+
+public interface MatchRecordRepository extends JpaRepository<MatchRecord, Long> {
+    List<MatchRecord> findByUserAIdOrUserBId(Long userAId, Long userBId);
+}
