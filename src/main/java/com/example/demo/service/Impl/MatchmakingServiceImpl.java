efbbbb9a517a789613226fbd40a7affcd666a61e
bala-amypo <bala@amypo.in>
2025-12-18 16:53:49 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/MatchmakingServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/MatchmakingServiceImpl.java
@@ -1,14 +1,3 @@
-package com.exmaple.demo.service.Impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import orng.springframework.stereotype.Service;
-import com.exmaple.demo.service.MatchmakingService;
-import com.example.demo.model.MatchRecord;
-
-@Service
-public class MatchmakingServiceImpl implements MatchmakingService{
-    @Autowired MatchRecordRepository ;
-}
 package com.exmaple.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,26 +9,20 @@ import com.exmaple.demo.model.MatchRecord;
 
 @Service
 public class MatchmakingServiceImpl implements MatchmakingService {
-
-    @Autowired
-    private MatchRecordRepository repo;
-
+    @Autowired MatchRecordRepository repo;
     @Override
     public MatchRecord generateMatch(Long userId) {
         throw new RuntimeException("No match found");
     }
-
     @Override
     public MatchRecord getMatchById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new RuntimeException("Match not found"));
     }
-
     @Override
     public List<MatchRecord> getMatchesForUser(Long userId) {
         return repo.findByUserAIdOrUserBId(userId, userId);
     }
-
     @Override
     public MatchRecord updateMatchStatus(Long id, String status) {
         MatchRecord match = getMatchById(id);
