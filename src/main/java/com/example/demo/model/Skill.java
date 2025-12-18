9a9ba82a39afa5c6f675e939d2b427c35451265a
bala-amypo <bala@amypo.in>
2025-12-18 15:11:23 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -16,43 +16,43 @@ public class Skill{
     private String category;
     private String description;
     private Boolean active;
-    public Long getId() {
+    public Long getId(){
         return id;
     }
-    public String getName() {
+    public String getName(){
         return name;
     }
-    public String getCategory() {
+    public String getCategory(){
         return category;
     }
-    public String getDescription() {
+    public String getDescription(){
         return description;
     }
-    public Boolean getActive() {
+    public Boolean getActive(){
         return active;
     }
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long id){
+        this.id=id;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name){
+        this.name=name;
     }
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategory(String category){
+        this.category=category;
     }
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String description){
+        this.description=description;
     }
-    public void setActive(Boolean active) {
-        this.active = active;
+    public void setActive(Boolean active){
+        this.active=active;
     }
     public Skill(){
     }
     public Skill(Long id, String name, String category, String description, Boolean active) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.description = description;
-        this.active = active;
+        this.id=id;
+        this.name=name;
+        this.category=category;
+        this.description=description;
+        this.active=active;
     }
 }
