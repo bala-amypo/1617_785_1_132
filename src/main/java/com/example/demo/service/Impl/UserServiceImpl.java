de8e89a7375b4d1989ba36cd2dcb16593aec968b
bala-amypo <bala@amypo.in>
2025-12-18 16:58:32 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,11 +1,23 @@
-package com.exmaple.demo.service.Impl;
+package com.exmaple.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import orng.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 import com.exmaple.demo.service.UserService;
-import com.example.demo.model.User;
+import com.exmaple.demo.repository.UserRepository;
+import com.exmaple.demo.model.User;
 
 @Service
-public class UserServieImpl implements UserService{
-    @Autowired UserRepository;
+public class UserServiceImpl implements UserService {
+    @Autowired UserRepository repo;
+    @Override
+    public User register(User user) {
+        if (repo.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+        return repo.save(user);
+    }
+    @Override
+    public User findByEmail(String email) {
+        return repo.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
