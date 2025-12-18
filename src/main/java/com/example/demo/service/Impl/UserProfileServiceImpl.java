9e01f1064fb0a2561073f90f591be1fef7704df4
bala-amypo <bala@amypo.in>
2025-12-18 16:02:29 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserProfileServiceImpl.java
@@ -14,3 +14,52 @@ public class UserProfileServieImpl implements UserProfileService{
     }
 
 } 
+package com.exmaple.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.exmaple.demo.service.UserProfileService;
+import com.exmaple.demo.repository.UserProfileRepository;
+import com.example.demo.model.UserProfile;
+
+@Service
+public class UserProfileServiceImpl implements UserProfileService{
+
+    @Autowired
+    private UserProfileRepository upr;
+
+    public UserProfile createUser(UserProfile user){
+        if(upr.existsByEmail(user.getEmail())){
+            throw new RuntimeException("Email already exists");
+        }
+        return upr.save(user);
+    }
+
+    public UserProfile updateUser(Long id,UserProfile user){
+        UserProfile existing=upr.findById(id)
+            .orElseThrow(()->new RuntimeException("UserProfile not found"));
+
+        existing.setFullName(user.getFullName());
+        existing.setEmail(user.getEmail());
+
+        return upr.save(existing);
+    }
+
+    public UserProfile getUserById(Long id){
+        return upr.findById(id)
+            .orElseThrow(()->new RuntimeException("UserProfile not found"));
+    }
+
+    public List<UserProfile> getAllUsers(){
+        return upr.findAll();
+    }
+
+    public void deactivateUser(Long id){
+        UserProfile user=upr.findById(id)
+            .orElseThrow(()->new RuntimeException("UserProfile not found"));
+
+        user.setActive(false);
+        upr.save(user);
+    }
+}
