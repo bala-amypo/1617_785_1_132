99171d4c31d51b46eb9d1aa5bca8ecba41a03504
bala-amypo <bala@amypo.in>
2025-12-25 17:53:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/example/demo/exception/EmailAlreadyExistsException.java
@@ -1,3 +1,5 @@
+package com.example.demo.exception;
+
 public class EmailAlreadyExistsException extends RuntimeException {
     public EmailAlreadyExistsException(String message) {
         super(message);
