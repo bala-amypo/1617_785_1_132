3aed85c18789079c7a057507f1e906ae93013bd9
bala-amypo <bala@amypo.in>
2025-12-25 17:24:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillOfferRepository.java
+++ src/main/java/com/example/demo/repository/SkillOfferRepository.java
@@ -1,11 +1,21 @@
 package com.example.demo.repository;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 import com.example.demo.model.SkillOffer;
 
-@Repository
-public interface SkillOfferRepository extends JpaRepository<SkillOffer,Long>{
-    List<SkillOffer> findByUserId(Long userId);
+import java.util.List;
+
+public interface SkillOfferRepository extends JpaRepository<SkillOffer, Long> {
+    List<SkillOffer> findBySkillIdAndActiveTrue(Long skillId);
 }
+// package com.example.demo.repository;
+
+// import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import java.util.List;
+// import com.example.demo.model.SkillOffer;
+
+// @Repository
+// public interface SkillOfferRepository extends JpaRepository<SkillOffer,Long>{
+//     List<SkillOffer> findByUserId(Long userId);
+// }
