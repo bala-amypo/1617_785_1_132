b1e4c92d2bb9d57800bf2884442b0c7a27b1a015
bala-amypo <bala@amypo.in>
2025-12-25 16:45:09 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillOfferServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillOfferServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
