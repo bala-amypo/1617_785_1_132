7913155c9bb73eb6f72b498944a92c4943fcfb55
bala-amypo <bala@amypo.in>
2025-12-25 16:20:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/MatchNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class MatchNotFoundException extends RuntimeException {
+    public MatchNotFoundException(String message) {
+        super(message);
+    }
+}
