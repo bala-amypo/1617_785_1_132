0d4797bc9a1271aca48aabbefd5b899890e94d05
bala-amypo <bala@amypo.in>
2025-12-25 18:05:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,29 +1,29 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
 
-    private final UserService service;
+//     private final UserService service;
 
-    public AuthController(UserService service) {
-        this.service = service;
-    }
+//     public AuthController(UserService service) {
+//         this.service = service;
+//     }
 
-    @PostMapping("/register")
-    public User register(@RequestBody User user) throws Exception {
-        return service.register(user);
-    }
+//     @PostMapping("/register")
+//     public User register(@RequestBody User user) throws Exception {
+//         return service.register(user);
+//     }
 
-    @PostMapping("/login")
-    public User login(@RequestParam String email) throws Exception {
-        return service.findByEmail(email);
-    }
-}
+//     @PostMapping("/login")
+//     public User login(@RequestParam String email) throws Exception {
+//         return service.findByEmail(email);
+//     }
+// }
 // package com.example.demo.controller;
 
 // import org.springframework.web.bind.annotation.*;
