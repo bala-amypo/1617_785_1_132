6cbaa2b4d2cce974378b2f61043c16b8adbc03af
bala-amypo <bala@amypo.in>
2025-12-25 17:30:48 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
@@ -1,3 +1,53 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Skill;
+import com.example.demo.repository.SkillRepository;
+import com.example.demo.service.SkillService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class SkillServiceImpl implements SkillService {
+
+    private final SkillRepository repo;
+
+    public SkillServiceImpl(SkillRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Skill createSkill(Skill skill) {
+        return repo.save(skill);
+    }
+
+    @Override
+    public Skill updateSkill(Long id, Skill skill) throws Exception {
+        Skill existing = getSkillById(id);
+        existing.setName(skill.getName());
+        existing.setCategory(skill.getCategory());
+        existing.setDescription(skill.getDescription());
+        return repo.save(existing);
+    }
+
+    @Override
+    public Skill getSkillById(Long id) throws Exception {
+        return repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Skill not found"));
+    }
+
+    @Override
+    public List<Skill> getAllSkills() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void deactivateSkill(Long id) throws Exception {
+        Skill existing = getSkillById(id);
+        existing.setActive(false);
+        repo.save(existing);
+    }
+}
 // package com.example.demo.service.Impl;
 
 // import org.springframework.beans.factory.annotation.Autowired;
