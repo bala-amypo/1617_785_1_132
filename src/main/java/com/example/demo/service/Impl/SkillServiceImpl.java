a017adbec167ce7dcf6490b1849b3aaf1671bd4e
bala-amypo <bala@amypo.in>
2025-12-26 13:41:57 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SkillServiceImpl.java
@@ -48,3 +48,29 @@
 //         repo.save(existing);
 //     }
 // }
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Skill;
+import com.example.demo.repository.SkillRepository;
+import com.example.demo.service.SkillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class SkillServiceImpl implements SkillService {
+    
+    @Autowired
+    private SkillRepository skillRepository;
+    
+    @Override
+    public Skill updateSkill(Long id, Skill skill) {
+        skill.setId(id);
+        return skillRepository.save(skill);
+    }
+    
+    @Override
+    public List<Skill> getAllSkills() {
+        return skillRepository.findAll();
+    }
+}
