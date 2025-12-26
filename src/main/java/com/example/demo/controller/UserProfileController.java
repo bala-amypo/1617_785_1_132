7accf2acbf798357db4cb9c64c640d7dcd5ce2a7
bala-amypo <bala@amypo.in>
2025-12-26 13:19:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserProfileController.java
+++ src/main/java/com/example/demo/controller/UserProfileController.java
@@ -42,3 +42,35 @@
 //         service.deactivateUser(id);
 //     }
 // }
+package com.example.demo.controller;
+
+import com.example.demo.model.UserProfile;
+import com.example.demo.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserProfileController {
+    
+    @Autowired
+    private UserProfileService userProfileService;
+    
+    @PostMapping("/users")
+    public ResponseEntity<UserProfile> create(@RequestBody UserProfile userProfile) {
+        UserProfile created = userProfileService.createUser(userProfile);
+        return ResponseEntity.ok(created);
+    }
+    
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserProfile> get(@PathVariable Long id) {
+        UserProfile user = userProfileService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+    
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deactivate(@PathVariable Long id) {
+        userProfileService.deactivateUser(id);
+        return ResponseEntity.ok().build();
+    }
+}
