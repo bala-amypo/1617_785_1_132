4b502b34a4bea128de29fcb0cf1b17185e2b88d3
bala-amypo <bala@amypo.in>
2025-12-26 13:28:07 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillOffer.java
+++ src/main/java/com/example/demo/model/SkillOffer.java
@@ -60,3 +60,27 @@
 //     public boolean isActive() { return active; }
 //     public void setActive(boolean active) { this.active = active; }
 // }
+package com.example.demo.model;
+
+public class SkillOffer {
+    private Long id;
+    private String experienceLevel;
+    private boolean active = true;
+    private UserProfile user;
+    private Skill skill;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public String getExperienceLevel() { return experienceLevel; }
+    public void setExperienceLevel(String experienceLevel) { this.experienceLevel = experienceLevel; }
+    
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+    
+    public UserProfile getUser() { return user; }
+    public void setUser(UserProfile user) { this.user = user; }
+    
+    public Skill getSkill() { return skill; }
+    public void setSkill(Skill skill) { this.skill = skill; }
+}
