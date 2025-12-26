f894746ffa480a098d14c9d9dd6effb3dee2452d
bala-amypo <bala@amypo.in>
2025-12-26 12:38:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -25,28 +25,3 @@ public class AuthController {
         return service.findByEmail(email);
     }
 }
-// package com.example.demo.controller;
-
-// import org.springframework.web.bind.annotation.*;
-// import com.example.demo.service.UserService;
-// import com.example.demo.model.User;
-
-// @RestController
-// public class AuthController {
-
-//     private final UserService service;
- 
-//     public AuthController(UserService service) {
-//         this.service = service;
-//     }
-
-//     @PostMapping("/auth/POST/register")
-//     public User register(@RequestBody User user) {
-//         return service.register(user);
-//     }
-
-//     @PostMapping("/auth/POST/login")
-//     public User login(@RequestParam String email) {
-//         return service.findByEmail(email);
-//     }
-// } 
--- src/main/java/com/example/demo/controller/SkillRequestController.java
+++ src/main/java/com/example/demo/controller/SkillRequestController.java
@@ -42,38 +42,3 @@ public class SkillRequestController {
         service.deactivateRequest(id);
     }
 }
-// package com.example.demo.controller;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-// import com.example.demo.service.SkillRequestService;
-// import com.example.demo.model.SkillRequest;
-
-// @RestController
-// public class SkillRequestController {
-//     private final SkillRequestService service;
-//     public SkillRequestController(SkillRequestService service){
-//         this.service=service;
-//     }
-//     @PostMapping("/api/skill-requests/POST")
-//     public SkillRequest create(@RequestBody SkillRequest request) {
-//         return service.createRequest(request);
-//     }
-//     @GetMapping("/api/skill-requests/GET/{id}")
-//     public SkillRequest getById(@PathVariable Long id) {
-//         return service.getRequestById(id);
-//     }
-//     @GetMapping("/api/skill-requests/GET/user/{userId}")
-//     public List<SkillRequest> getByUser(@PathVariable Long userId) {
-//         return service.getRequestsByUser(userId);
-//     }
-//     @PutMapping("/api/skill-requests/PUT/{id}")
-//     public SkillRequest update(@PathVariable Long id, @RequestBody SkillRequest request) {
-//         return service.updateRequest(id, request);
-//     }
-//     @PutMapping("/api/skill-requests/PUT/{id}/deactivate")
-//     public void deactivate(@PathVariable Long id) {
-//         service.deactivateRequest(id);
-//     }
-// }
