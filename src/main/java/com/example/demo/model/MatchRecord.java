cc204cd8e627ad8fdaf75a4236f975beaf83d300
bala-amypo <bala@amypo.in>
2025-12-26 18:12:38 +0000

    User commit

--- src/main/java/com/example/demo/model/MatchRecord.java
+++ src/main/java/com/example/demo/model/MatchRecord.java
@@ -65,3 +65,44 @@
 //     public String getStatus() { return status; }
 //     public void setStatus(String status) { this.status = status; }
 // }
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class MatchRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+
+    @ManyToOne private UserProfile userA;
+    @ManyToOne private UserProfile userB;
+
+    @ManyToOne private Skill skillOfferedByA;
+    @ManyToOne private Skill skillOfferedByB;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public UserProfile getUserA() { return userA; }
+    public void setUserA(UserProfile userA) { this.userA = userA; }
+
+    public UserProfile getUserB() { return userB; }
+    public void setUserB(UserProfile userB) { this.userB = userB; }
+
+    public Skill getSkillOfferedByA() { return skillOfferedByA; }
+    public void setSkillOfferedByA(Skill skillOfferedByA) {
+        this.skillOfferedByA = skillOfferedByA;
+    }
+
+    public Skill getSkillOfferedByB() { return skillOfferedByB; }
+    public void setSkillOfferedByB(Skill skillOfferedByB) {
+        this.skillOfferedByB = skillOfferedByB;
+    }
+}
