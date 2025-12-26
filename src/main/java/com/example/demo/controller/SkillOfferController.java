44cba5d502ed7527835eaca27700b903f7fd7ea2
bala-amypo <bala@amypo.in>
2025-12-26 13:18:35 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillOfferController.java
+++ src/main/java/com/example/demo/controller/SkillOfferController.java
@@ -42,3 +42,29 @@
 //         service.deactivateOffer(id);
 //     }
 // }
+package com.example.demo.controller;
+
+import com.example.demo.model.SkillOffer;
+import com.example.demo.service.SkillOfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SkillOfferController {
+    
+    @Autowired
+    private SkillOfferService skillOfferService;
+    
+    @PostMapping("/offers")
+    public ResponseEntity<SkillOffer> create(@RequestBody SkillOffer skillOffer) {
+        SkillOffer created = skillOfferService.createOffer(skillOffer);
+        return ResponseEntity.ok(created);
+    }
+    
+    @GetMapping("/offers/{id}")
+    public ResponseEntity<SkillOffer> get(@PathVariable Long id) {
+        SkillOffer offer = skillOfferService.getOfferById(id);
+        return ResponseEntity.ok(offer);
+    }
+}
